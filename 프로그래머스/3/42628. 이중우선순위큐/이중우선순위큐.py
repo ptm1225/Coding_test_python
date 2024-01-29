17b98345a169a15b629f6b62ecd88c17d7f97059
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-01-30 00:03:48 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,17 +1,17 @@
 def solution(operations):
     arr = []
     for i, v in enumerate(operations):
-        if operations[i].startswith("I"):
+        op, num = operations[i].split(' ')
+        if op == "I":
             arr.append(int(v[2:]))
             arr.sort()
-            print(arr)
-        else:
+        elif op == "D":
             if len(arr) == 0:
                 continue
                 
-            if operations[i] == "D 1":
+            if num == "1":
                 arr.pop()
-            elif operations[i] == "D -1":
+            elif num == "-1":
                 arr = arr[1:]
     
     if len(arr) == 0:
