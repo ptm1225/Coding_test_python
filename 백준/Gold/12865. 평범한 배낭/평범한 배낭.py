28ca0ea42e6d82c1f5c819d5af1f68f3f8ea4dfa
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-02-06 18:50:04 +0900

    [Gold V] Title: 평범한 배낭, Time: 356 ms, Memory: 189776 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,16 @@
+arr = []
+n, k = map(int, input().split())
+for _ in range(n):
+  w, v = map(int, input().split())
+  arr.append((w, v))
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+for i in range(1, n + 1):
+  for j in range(1, k + 1):
+    w, v = arr[i - 1]
+    if j < w:
+      dp[i][j] = dp[i - 1][j]
+    else:
+      dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+
+print(dp[n][k])
