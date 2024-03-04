ed0703200c71d4492813d8cf952ffb04af5048bd
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-04 22:31:40 +0900

    [Bronze II] Title: 저항, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,5 @@
+color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+one=str(color.index(input()))
+two=str(color.index(input()))
+three='0'*color.index(input())
+print(int(''.join(one+two+three)))
