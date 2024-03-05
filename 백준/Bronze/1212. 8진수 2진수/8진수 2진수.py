79c85509e20b9ce7bd314de1bb190331c1065dea
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-05 10:37:37 +0900

    [Bronze II] Title: 8진수 2진수, Time: 228 ms, Memory: 114416 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.py
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.py
@@ -1,5 +1,8 @@
-result = []
+is_first = True
 for i in input():
     a = bin(int(i))[2:]
-    result.append('0'*(3-len(a)) + a)
-print(int(''.join(result)))
+    if is_first:
+        is_first = False
+        print(a, end='')
+    else:
+        print('0'*(3-len(a)) + a, end='')
