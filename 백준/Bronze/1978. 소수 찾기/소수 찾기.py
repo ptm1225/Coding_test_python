619e0df149d28a2d01c2e59e2241fa8569319b8e
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-07 23:24:54 +0900

    [Bronze II] Title: 소수 찾기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,10 @@
+n = int(input())
+count = 0
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+print(sum([is_prime(i) for i in list(map(int, input().split()))]))
