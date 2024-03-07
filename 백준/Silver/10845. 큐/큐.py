f3cff7a10552b3e8b9298965f684c705915ebbe2
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-08 00:27:08 +0900

    [Silver IV] Title: 큐, Time: 172 ms, Memory: 114088 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+n = int(input())
+queue = []
+arr = []
+for _ in range(n):
+    arr.append(input().split())
+for c in arr:
+    if c[0] == 'push':
+        queue.append(int(c[1]))
+    elif c[0] == 'pop':
+        if queue:
+            print(queue.pop(0))
+        else:
+            print(-1)
+    elif c[0] == 'size':
+        print(len(queue))
+    elif c[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif c[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif c[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
