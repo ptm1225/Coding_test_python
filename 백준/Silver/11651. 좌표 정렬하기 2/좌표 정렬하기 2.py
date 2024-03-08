6aa9dadcabc9ebe6bb9bdd93818f1aea819cd50f
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-09 01:02:12 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 452 ms, Memory: 126716 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+arr.sort(key=lambda x:x[0])
+arr.sort(key=lambda x:x[1])
+for a in arr:
+    print(*a)
