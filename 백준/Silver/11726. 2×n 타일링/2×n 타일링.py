0613cd60900c8f628439091f27d3a7393e0566c4
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-13 01:07:08 +0900

    [Silver III] Title: 2×n 타일링, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * (n+1)
+for i in range(1, n+1):
+    if i < 3:
+        dp[i] = i
+    else:
+        dp[i] = dp[i-1] + dp[i-2]
+print(dp[n]%10007)
