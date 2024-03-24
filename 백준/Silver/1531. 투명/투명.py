d3de9c1f42210bc1c91975fe170638ed058cff25
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-03-24 23:30:15 +0900

    [Silver V] Title: 투명, Time: 124 ms, Memory: 110668 KB -BaekjoonHub

---
+++ 백준/Silver/1531. 투명/투명.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+arr = [[0] * 100 for _ in range(100)]
+for _ in range(n):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1-1, x2):
+        for j in range(y1-1, y2):
+            arr[i][j] += 1
+
+result = 0
+for i in range(100):
+    for j in range(100):
+        if arr[i][j] > m:
+            result += 1
+print(result)
