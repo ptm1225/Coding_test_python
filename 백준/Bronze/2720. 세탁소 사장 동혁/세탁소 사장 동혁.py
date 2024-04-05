771a0523d6fdd166c7ce4d0516e2484f256ee133
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-04-05 13:06:01 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 164 ms, Memory: 110560 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(n//25, end=' ')
+    n %= 25
+    print(n//10, end=' ')
+    n %= 10
+    print(n//5, end=' ')
+    n %= 5
+    print(n)
