04d0a13900d92347cc03ce0356f74eb98e8f5d0b
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-05-12 20:43:23 +0900

    [Gold V] Title: 줄어드는 수, Time: 132 ms, Memory: 110856 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = []
+result = []
+
+def dfs():
+    if len(arr) > 0:
+        result.append(int("".join(map(str, arr))))
+        
+    for i in range(10): 
+        if len(arr) == 0 or arr[-1] > i:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+dfs()
+result.sort()
+if n <= len(result):
+    print(result[n-1])
+else:
+    print(-1)
