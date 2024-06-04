f44da3db493d0d446fa5426ffc4c2cdbe6b0cd70
Taemin Park <109125108+ptm1225@users.noreply.github.com>
2024-06-04 21:36:51 +0900

    [Gold V] Title: 물병, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/1052. 물병/물병.py
+++ 백준/Gold/1052. 물병/물병.py
@@ -5,7 +5,8 @@ n, k = map(int, input().split())
 count = 0
 
 while bin(n).count('1') > k:
-    n += 1
-    count += 1
+    i = bin(n)[::-1].index('1')
+    n += 2 ** i
+    count += 2 ** i
 
 print(count)
